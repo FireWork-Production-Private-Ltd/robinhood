75a28717cd8b941ccc6c6501688846123bf1b431
utsav <75293077+up1512001@users.noreply.github.com>
2022-07-08 21:27:43 +0530

    header updated

--- src/Header.js
+++ src/Header.js
@@ -6,15 +6,24 @@ function Header() {
     <div className='header__wrapper'>
         {/* Logo */}
         <div className='header__logo'>
-            <img src={Logo} width={25} />
+            <img src={Logo} width={25} alt="Logo" />
         </div>
         {/* Search Bar */}
         <div className='header__search'>
             <div className='header__searchContainer'>
-                
+                <input placeholder='Search' type="text" ></input>
             </div>
         </div>
         {/* Menu items */}
+        <div className='header__menuItems'>
+            <a href="#">Free Stocks</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Cahs</a>
+            <a href="#">Messages</a>
+            <a href="#">Account</a>
+
+        </div>
+
 
     </div>
   )
